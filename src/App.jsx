0259051a78831fd753fffffe27130cd353ebc492
kevdev <kevlays@gmail.com>
2023-03-28 19:03:37 -0700

    Added fetchAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-
 import Navbar from './components/Navbar';
+import Feed from './components/Feed';
 
 function App() {
   return (
     <>
     <Navbar/>
-    test
     <Routes>
-      <Route path='/' element={'asdf'}/>
+      <Route path='/' element={<Feed/>}/>
     </Routes>
     </>
   );
