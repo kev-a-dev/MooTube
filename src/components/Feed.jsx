ea4c339f6799f211c026d6008aa901ada39c3ee0
kevdev <kevlays@gmail.com>
2023-04-03 22:47:32 -0700

    i fixed watch page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,43 +3,68 @@ import { Link } from 'react-router-dom';
 
 import './Feed.css';
 import axios from 'axios';
-
 import Sidebar from './Sidebar'
 
-export default function Feed() {
 
+export default function Feed() {
+  
   const [category, setCategory] = useState('Trending')
   const [videos, setVideos] = useState([])
 
-  function formatNumber(num) {
-    if (num >= 1000000000) {
-      return (num / 1000000000).toFixed(1) + 'B views';
-    }
-    if (num >= 1000000) {
-      return (num / 1000000).toFixed(1) + 'M views';
-    }
-    if (num >= 1000) {
-      return (num / 1000).toFixed(1) + 'K views';
-    }
-    return num + ' views';
-  }
+  // FORMAT TIME
+  function formatTime(dateString) {
+    const date = new Date(dateString);
+    const now = new Date();
+    const diffMs = now - date;
+    const diffSeconds = Math.floor(diffMs / 1000);
+    const diffMinutes = Math.floor(diffSeconds / 60);
+    const diffHours = Math.floor(diffMinutes / 60);
+    const diffDays = Math.floor(diffHours / 24);
+    const diffWeeks = Math.floor(diffDays / 7);
+    const diffMonths = Math.floor(diffDays / 30);
+    const diffYears = Math.floor(diffDays / 365);
   
+    if (diffSeconds < 60) {
+      return `${diffSeconds} seconds ago`;
+    } else if (diffMinutes < 60) {
+      return `${diffMinutes} minutes ago`;
+    } else if (diffHours < 24) {
+      return `${diffHours} hours ago`;
+    } else if (diffDays < 7) {
+      return `${diffDays} days ago`;
+    } else if (diffWeeks < 4) {
+      return `${diffWeeks} weeks ago`;
+    } else if (diffMonths < 12) {
+      return `${diffMonths} months ago`;
+    } else {
+      return `${diffYears} years ago`;
+    }
+  }
+
+  // FETCH VIDEO CATEGORY
   useEffect(() => {
     const options = {
       method: 'GET',
-      url: 'https://youtube-v38.p.rapidapi.com/search/',
-      params: {q: category, hl: 'en', gl: 'US'},
+      url: 'https://youtube-v31.p.rapidapi.com/search',
+      params: {
+        q: category,
+        part: 'snippet,id',
+        regionCode: 'US',
+        maxResults: '50',
+        order: 'date'
+      },
       headers: {
         'X-RapidAPI-Key': '583627f704msh19c07864199cf13p1b7dadjsnc2e1d0cc8354',
-        'X-RapidAPI-Host': 'youtube-v38.p.rapidapi.com'
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
       }
     };
 
     axios.request(options).then(function (response) {
-      setVideos(response.data.contents);
+      setVideos(response.data.items);
     })
-    
   }, [category]);
+
+  console.log(videos)
 
   return (
     <section id="feed">
@@ -54,21 +79,21 @@ export default function Feed() {
 
           <div className="videos-container">
             {videos.map((video, index) => (
-              <Link to={{ pathname: `/Watch/${video.video.videoId}`, 
-                    state: { publishedTimeText: video.video.publishedTimeText } }} 
+              video && 
+              <Link to={ `/Watch/${video.id.videoId}`}
                     className='link' 
                     key={index}
               >
                 <div className='video'>
-                  <img src={video.video.thumbnails[0].url} alt="" className="video-img" />
+                  <img src={video.snippet.thumbnails.high.url} alt="" className="video-img" />
                   <div className="video-info-wrapper">
-                    <img src={video.video.author.avatar[0].url} alt="" className='avatar'/>
+                    {/* <img src={video.video.author.avatar[0].url} alt="" className='avatar'/> */}
                     <div className="title-channel-stats-wrapper">
-                      <h3 className="title">{video.video.title.slice(0,60)}</h3>
-                      <h3 className="channel">{video.video.author.title}</h3>
+                      <h3 className="title">{video.snippet.title.slice(0,60)}</h3>
+                      <h3 className="channel">{video.snippet.channelTitle}</h3>
                       <div className="stats-wrapper">
-                        <h3 className="views">{formatNumber(video.video.stats.views)}</h3>
-                        <h3 className="date">{video.video.publishedTimeText}</h3>
+                        {/* <h3 className="views">{formatNumber(video.video.stats.views)}</h3> */}
+                        <h3 className="date">{formatTime(video.snippet.publishTime)}</h3>
                       </div>
                     </div>
                   </div>
--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -5,14 +5,40 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
+import moo from '../assets/moo.png';
 
 export default function Watch() {
 
   const { id } = useParams();
   const [videos, setVideos] = useState([]);
   const [videoDetail, setVideoDetail] = useState([]);
-  const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState(null);
 
+  // FORMAT PUBLISH DATE
+  function formatPublishDate(publishDate) {
+    const datePublished = new Date(publishDate);
+    const currentDate = new Date();
+    const diffTime = Math.abs(currentDate - datePublished);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  
+    if (diffDays === 1) {
+      return '1 day ago';
+    } else if (diffDays < 7) {
+      return `${diffDays} days ago`;
+    } else if (diffDays < 14) {
+      return '1 week ago';
+    } else if (diffDays < 30) {
+      const weeksAgo = Math.floor(diffDays / 7);
+      return `${weeksAgo} weeks ago`;
+    } else if (diffDays < 365) {
+      const monthsAgo = Math.floor(diffDays / 30);
+      return `${monthsAgo} months ago`;
+    } else {
+      const yearsAgo = Math.floor(diffDays / 365);
+      return `${yearsAgo} years ago`;
+    }
+  }
+  
   // FORMAT NUMBERS
   function formatNumber(num) {
     if (num >= 1000000000) {
@@ -28,74 +54,83 @@ export default function Watch() {
   }
 
   // TOGGLE DESCRIPTION
-  function handleToggleOpen() {
+  function handleToggleOpen(event) {
     document.body.querySelector('.description').classList.toggle('open')
+    
+    if (event.target.innerHTML === 'Show less'){
+      event.target.innerHTML = 'Show more';
+    } else if (event.target.innerHTML === 'Show more') {
+      event.target.innerHTML = 'Show less';
+    }
   }
   
-  // VIDEO DETAILS
+  // FETCH VIDEO DETAILS
   useEffect(() => {
-
     const options = {
       method: 'GET',
-      url: 'https://youtube-v38.p.rapidapi.com/video/details/',
-      params: {id: id, hl: 'en', gl: 'US'},
+      url: 'https://youtube-v31.p.rapidapi.com/videos',
+      params: {part: 'contentDetails,snippet,statistics', id: id},
       headers: {
         'X-RapidAPI-Key': '583627f704msh19c07864199cf13p1b7dadjsnc2e1d0cc8354',
-        'X-RapidAPI-Host': 'youtube-v38.p.rapidapi.com'
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
       }
     };
     
     axios.request(options).then(function (response) {
-      setVideoDetail(response.data);
+      setVideoDetail(response.data.items[0]);
     })
-
-  }, [id]);  
-
-  // VIDEO COMMENTS
+    
+  }, [id]);
+  
+  // FETCH VIDEO COMMENTS
   useEffect(() => {
-
     const options = {
       method: 'GET',
-      url: 'https://youtube-v38.p.rapidapi.com/video/comments/',
-      params: {id: id, hl: 'en', gl: 'US'},
+      url: 'https://youtube-v31.p.rapidapi.com/commentThreads',
+      params: {part: 'snippet', videoId: id, maxResults: 50},
       headers: {
         'X-RapidAPI-Key': '583627f704msh19c07864199cf13p1b7dadjsnc2e1d0cc8354',
-        'X-RapidAPI-Host': 'youtube-v38.p.rapidapi.com'
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
       }
     };
     
     axios.request(options).then(function (response) {
-      setComments(response.data.comments);
+      setComments(response.data.items);
     })
-
-  }, [id]);  
-
-  console.log(comments)
-
-  // VIDEO SUGGESTIONS
+    
+  }, [id]);
+  
+  //  FETCH VIDEO SUGGESTIONS
   useEffect(() => {
-
     const options = {
       method: 'GET',
-      url: 'https://youtube-v38.p.rapidapi.com/video/related-contents/',
-      params: {id: id, hl: 'en', gl: 'US'},
+      url: 'https://youtube-v31.p.rapidapi.com/search',
+      params: {
+        relatedToVideoId: id,
+        part: 'id,snippet',
+        type: 'video',
+        maxResults: '50'
+      },
       headers: {
         'X-RapidAPI-Key': '583627f704msh19c07864199cf13p1b7dadjsnc2e1d0cc8354',
-        'X-RapidAPI-Host': 'youtube-v38.p.rapidapi.com'
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
       }
     };
     
     axios.request(options).then(function (response) {
-      setVideos(response.data.contents);
+      setVideos(response.data.items);
     })
 
   }, [id]);
+
+  if (comments === null) {
+    return <div>Loading...</div>;
+  }
   
   return (
     <div>
         <section id="watch">
             <div className="container">
-
               {/* VIDEO */}
               <div className="react-player-container">
                 <ReactPlayer
@@ -104,35 +139,40 @@ export default function Watch() {
                   height='100%'
                   url={`https://www.youtube.com/watch?v=${id}`}
                   controls={true}
-                />
+                  />
                 {/* INFO */}
-                {videoDetail.author &&(
+                
+                {videoDetail.snippet && (
                 <>
-                <h3 className="title">{videoDetail.title}</h3>
+                <h3 className="title">{videoDetail.snippet.title}</h3>
 
                 <div className="avatar-channel-wrapper">
                   <div className="avatar-wrapper">
-                    <img src={videoDetail.author.avatar[2].url} alt="" className='avatar'/>
+                    <img src={videoDetail.snippet.thumbnails.default.url} alt="" className='avatar'/>
                     <div className="channel-sub-wrapper">
-                      <h3 className="channel">{videoDetail.author.title}</h3>
-                      <h3 className="subscribers">{videoDetail.author.stats.subscribersText}</h3>
+                      <h3 className="channel">{videoDetail.snippet.channelTitle}</h3>
+                      {/* <h3 className="subscribers">{videoDetail.author.stats.subscribersText}</h3> */}
                     </div>
                   </div>
                     
                   <div className="view-date-wrapper">
-                    <div className="views">{formatNumber(videoDetail.stats.views)}</div>
-                    <div className="date">{videoDetail.publishedDate}</div>
+                    <div className="views">{formatNumber(videoDetail.statistics.viewCount)}</div>
+                    <div className="date">{formatPublishDate(videoDetail.snippet.publishedAt)}</div>
                   </div>
                   
                 </div>
                 
                 {/* DESCRIPTION */}
-                <div 
-                  className="description-container"
-                  onClick={handleToggleOpen}
-                >
-                  {videoDetail.description &&
-                    <h5 className="description">{videoDetail.description}</h5>
+                <div className="description-container">
+                  {videoDetail &&
+                    <>
+                      <h5 className="description">{videoDetail.snippet.description}</h5>
+                      <span 
+                        className="des-toggle"
+                        onClick={handleToggleOpen}>
+                        Show more
+                      </span>
+                    </>
                   }
                 </div>
                 </>
@@ -140,18 +180,27 @@ export default function Watch() {
 
                 {/* COMMENTS */}
                 <div className="comments-container">
+                  <h3 className="recent">Recent Comments</h3>
+
                   {comments.map((comment, index) => (
-                    comment &&
+                    comment.snippet &&
                     <div className="comment" key={index}>
                       <div className="img-wrapper">
-                        <img src={comment.author.avatar[0].url} alt="" className="avatar" />
+                        <img src={comment.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" className="avatar" />
                       </div>
-                      <div className="title-date-wrapper">
-                        <h4 className="title">{comment.author.title}</h4>
-                        <h4 className="date">{comment.publishedTimeText}</h4>
+
+                      <div className="title-date-content-wrapper">
+                        <div className="title-date-wrapper">
+                          <h4 className="title">{comment.snippet.topLevelComment.snippet.authorDisplayName}</h4>
+                          <h4 className="date">{formatPublishDate(comment.snippet.topLevelComment.snippet.publishedAt)}</h4>
+                        </div>
+                        <h4 className="content">{comment.snippet.topLevelComment.snippet.textDisplay}</h4>
+
+                        <h4 className="votes">
+                          <i className="fa-regular fa-heart"></i>
+                          {comment.snippet.topLevelComment.snippet.likeCount}
+                        </h4>
                       </div>
-                      <h4 className="content">{comment.author.content}</h4>
-                      {/* <h4 className="likes">{comment.author.stats.votes}</h4> */}
                     </div>
                   ))}
                 </div>
@@ -161,20 +210,21 @@ export default function Watch() {
               {/* VIDEO SUGGESTIONS */}
               <div className="videos-container">
                 {videos.map((video, index) => (
-                  <Link to={`/Watch/${video.video.videoId}`} 
-                    className='link' 
-                    key={index}
+                  
+                  <Link to={ `/Watch/${video.id.videoId}`}
+                        className='link' 
+                        key={index}
                   >
                     <div className='video'>
-                      <img src={video.video.thumbnails[1].url} alt="" className="video-img" />
+                      <img src={video.snippet.thumbnails.high.url} alt="" className="video-img" />
                       <div className="video-info-wrapper">
-                        <img src={video.video.author.avatar[0].url} alt="" className='avatar'/>
+                        {/* <img src={video.video.author.avatar[0].url} alt="" className='avatar'/> */}
                         <div className="title-channel-stats-wrapper">
-                          <h3 className="title">{video.video.title.slice(0,60)}</h3>
-                          <h3 className="channel">{video.video.author.title}</h3>
+                          <h3 className="title">{video.snippet.title.slice(0,60)}</h3>
+                          <h3 className="channel">{video.snippet.channelTitle}</h3>
                           <div className="stats-wrapper">
-                            <h3 className="views">{formatNumber(video.video.stats.views)}</h3>
-                            <h3 className="date">{video.video.publishedTimeText}</h3>
+                            {/* <h3 className="views">{formatNumber(video.video.stats.views)}</h3> */}
+                            <h3 className="date">{formatPublishDate(video.snippet.publishTime)}</h3>
                           </div>
                         </div>
                       </div>
